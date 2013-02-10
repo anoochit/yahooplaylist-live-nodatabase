1cddbb889b5e18e3b5d5ee0cf7896ccc75e0e7a1
redlinesoft <anoochit@gmail.com>
2013-02-10 11:17:46 +0700

    initial project

---
+++ gen/net/redlinesoft/app/yahooplaylistlive/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.redlinesoft.app.yahooplaylistlive;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
